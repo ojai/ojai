790baae5fceccd119598009e4503f1f30c92f22f
Santanu Chatterjee <schatterjee@maprtech.com>
2016-02-22 14:48:53 -0800

    MAPR-18724 : Support for Compression in JSON M/R framework.

--- map-reduce/src/main/java/org/ojai/json/mapreduce/JSONFileRecordReader.java
+++ map-reduce/src/main/java/org/ojai/json/mapreduce/JSONFileRecordReader.java
@@ -16,13 +16,17 @@
 package org.ojai.json.mapreduce;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.compress.CodecPool;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.Decompressor;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
@@ -30,10 +34,13 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.ojai.Document;
 import org.ojai.json.Json;
 import org.ojai.json.impl.JsonDocumentStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JSONFileRecordReader extends RecordReader<LongWritable, Document> {
 
-  private FSDataInputStream inputStream;
+  private static Logger LOG = LoggerFactory.getLogger(JSONFileRecordReader.class);
+  private InputStream inputStream;
   private JsonDocumentStream documentStream;
   private Iterator<Document> it;
   private long documentCount;
@@ -42,6 +49,7 @@ public class JSONFileRecordReader extends RecordReader<LongWritable, Document> {
   private long currentPos;
   private long start;
   private long end;
+  private Decompressor decompressor;
 
   @Override
   public void close() throws IOException {
@@ -51,8 +59,15 @@ public class JSONFileRecordReader extends RecordReader<LongWritable, Document> {
       throw new IOException(
           "Error closing document Stream in JsonFileRecordReader");
     }
-    if (inputStream != null) {
-      inputStream.close();
+    try {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+    } finally {
+      if (decompressor != null) {
+        CodecPool.returnDecompressor(decompressor);
+        decompressor = null;
+      }
     }
   }
 
@@ -96,6 +111,13 @@ public class JSONFileRecordReader extends RecordReader<LongWritable, Document> {
     final Path path = split.getPath();
     FileSystem fs = path.getFileSystem(job);
     inputStream = fs.open(path);
+
+    CompressionCodec codec = new CompressionCodecFactory(job).getCodec(path);
+
+    if (codec != null) {
+      decompressor = CodecPool.getDecompressor(codec);
+      inputStream = codec.createInputStream(inputStream, decompressor);
+    }
 
     start = split.getStart();
     end = start + split.getLength();
