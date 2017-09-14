a4c79524b410f38615a089ad37119045117d2390
Aditya <aditya@mapr.com>
2017-09-13 23:17:30 -0700

    Added a utility class `EmptyDocumentStream` for cases where an empty `DocumentStream` has to be returned.

---
+++ core/src/main/java/org/ojai/util/EmptyDocumentStream.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2017 MapR, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ojai.util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.ojai.Document;
+import org.ojai.DocumentListener;
+import org.ojai.DocumentReader;
+import org.ojai.DocumentStream;
+import org.ojai.exceptions.OjaiException;
+
+/**
+ * A DocumentStream that returns no documents.
+ */
+public class EmptyDocumentStream implements DocumentStream {
+  private boolean isUsed;
+  private boolean isClosed;
+
+  @Override
+  public Iterator<Document> iterator() {
+    checkState();
+    return new EmptyIterator<Document>();
+  }
+
+  @Override
+  public void streamTo(DocumentListener listener) {
+    checkState();
+    listener.eos();
+  }
+
+  @Override
+  public Iterable<DocumentReader> documentReaders() {
+    checkState();
+    return new EmptyIterable<DocumentReader>();
+  }
+
+  @Override
+  public void close() throws OjaiException {
+    isClosed = true;
+  }
+
+  private void checkState() {
+    checkOpen();
+    if (isUsed) {
+      throw new IllegalStateException("DocumentStream has already been used once");
+    }
+  }
+  private void checkOpen() {
+    if (isClosed) {
+      throw new IllegalStateException("DocumentStream is already closed");
+    }
+  }
+
+  /**
+   * An empty Iterator that returns nothing.
+   */
+  private static class EmptyIterator<T> implements Iterator<T> {
+    @Override
+    public boolean hasNext() {
+      return false;
+    }
+
+    @Override
+    public T next() {
+      throw new NoSuchElementException();
+    }
+  }
+
+  /**
+   * An empty Iterable that returns nothing.
+   */
+  private static class EmptyIterable<T> implements Iterable<T> {
+    @Override
+    public Iterator<T> iterator() {
+      return new EmptyIterator<T>();
+    }
+  }
+
+}
---
+++ core/src/test/java/org/ojai/tests/util/TestEmptyDocumentStream.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2017 MapR, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ojai.tests.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Test;
+import org.ojai.Document;
+import org.ojai.DocumentListener;
+import org.ojai.DocumentStream;
+import org.ojai.util.EmptyDocumentStream;
+
+public class TestEmptyDocumentStream {
+
+  @Test
+  public void test_EmptyDocumentStream() {
+    // Document iterator
+    final DocumentStream stream1 = new EmptyDocumentStream();
+    assertFalse(stream1.iterator().hasNext());
+    stream1.close();
+
+    // DocumentReader iterator
+    final DocumentStream stream2 = new EmptyDocumentStream();
+    assertFalse(stream2.documentReaders().iterator().hasNext());
+    stream2.close();
+
+    // streamTo
+    final AtomicReference<Boolean> eosCalled = new AtomicReference<Boolean>(false);
+    final AtomicReference<Boolean> failedCalled = new AtomicReference<Boolean>(false);
+    final AtomicReference<Boolean> documentArrivedCalled = new AtomicReference<Boolean>(false);
+    final DocumentStream stream3 = new EmptyDocumentStream();
+    stream3.streamTo(new DocumentListener() {
+      @Override
+      public void eos() {
+        eosCalled.set(true);
+      }
+
+      @Override
+      public void failed(Exception e) {
+        failedCalled.set(true);
+      }
+
+      @Override
+      public boolean documentArrived(Document document) {
+        documentArrivedCalled.set(true);
+        return false;
+      }
+    });
+    assertTrue(eosCalled.get());
+    assertFalse(failedCalled.get());
+    assertFalse(documentArrivedCalled.get());
+    stream3.close();
+  }
+
+}
